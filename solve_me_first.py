53eb55a90d6256d4b24611d4d03a1f2acb3e2bbe
teresanair2-web <teresanair2@gmail.com>
2026-01-14 20:13:20 +0530

    Create solve_me_first.py

---
+++ solve_me_first.py
@@ -0,0 +1,11 @@
+# Problem: Solve Me First
+# Platform: HackerRank
+
+def solveMeFirst(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    a = int(input())
+    b = int(input())
+    result = solveMeFirst(a, b)
+    print(result)
